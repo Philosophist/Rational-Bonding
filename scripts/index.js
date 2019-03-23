db6701d0effa0d5fb6857246695ca5aaa8d23bcb
Steve <srp2120@gmail.com>
2019-03-22 22:16:10 -0700

    We have achieved orbit!

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,23 @@
-// Registering component in foo-component.js
-AFRAME.registerComponent('s-orbital', {  
+/* global AFRAME */
+
+AFRAME.registerComponent('s-orbital', {
+
   init: function () {
-    this.throttledOrbit = AFRAME.utils.throttle(this.everyOrbit, 250, this);
-    this.throttledHalfOrbit = AFRAME.utils.throttle(this.everyHalfOrbit, 125, this);
-  },
-
-  everyOrbit: function () {
-    if(this.animation.property.dir==='normal'){
-      this.animation.property.dir='reverse';
-    }else{this.animation.property.dir='normal';}
-    this.animation();
-  },
-
-  everyHalfOrbit: function () {
-    if(this.animation__rad.property.dir==='normal'){
-      this.animation__rad.property.dir='reverse';
-    }else{this.animation__rad.property.dir='normal';}
-    this.animation__rad();
-  },
-  
-  tick: function (t, dt) {
-    this.throttledOrbit();
-    this.throttledHalfOrbit();
+    
+    var self = this;
+    
+    setInterval(function () {
+      self.el.emit('animate-rad');
+      
+      if ( self.el.is('cycle') ) {
+        self.el.removeState('cycle');
+        return;
+      }
+      
+      self.el.emit('animate-pos');
+      self.el.addState('cycle');
+      
+    }, 250);
+    
   }
-});
+}
